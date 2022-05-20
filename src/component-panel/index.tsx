4a631db34f74a84fd23c3a0485da42c213f1eed2
MadCcc <1075746765@qq.com>
2022-05-20 18:42:09 +0800

    feat: icon to close component tree

--- src/component-panel/index.tsx
+++ src/component-panel/index.tsx
@@ -1,11 +1,11 @@
 import type { FC } from 'react';
-import React from 'react';
-import ButtonDemo from '../demos/button';
+import React, { useState } from 'react';
 import ComponentTree from './ComponentTree';
 import makeStyle from '../utils/makeStyle';
 import classNames from 'classnames';
+import { MenuFoldOutlined } from '@ant-design/icons';
 
-const useStyle = makeStyle('ComponentPanel', () => ({
+const useStyle = makeStyle('ComponentPanel', (token) => ({
   '.component-panel': {
     boxShadow:
       '0 2px 4px 0 rgba(0,0,0,0.05), 0 1px 2px 0 rgba(25,15,15,0.07), 0 0 1px 0 rgba(0,0,0,0.08)',
@@ -14,16 +14,65 @@ const useStyle = makeStyle('ComponentPanel', () => ({
     borderRadius: 6,
     height: '100%',
     overflow: 'hidden',
+
+    '.component-panel-main': {
+      display: 'flex',
+      flexDirection: 'column',
+      backgroundColor: token.colorBgContainer,
+      flex: 1,
+
+      '.component-panel-head': {
+        padding: `${token.padding}px ${token.paddingSM}px`,
+        flex: 'none',
+        backgroundColor: token.colorBgComponent,
+      },
+
+      '.component-panel-toggle-side-icon': {
+        '.anticon': {
+          color: token.colorAction,
+          transition: `color ${token.motionDurationMid}`,
+
+          '&:hover': {
+            color: token.colorActionHover,
+          },
+        },
+      },
+    },
+
+    '.component-panel-side': {
+      flex: 'none',
+      width: 160,
+      overflow: 'hidden',
+      transition: `transform ${token.motionDurationMid}, width ${token.motionDurationMid}`,
+    },
+
+    '.component-panel-side.component-panel-side-hidden': {
+      width: 0,
+      transform: 'translateX(-160px)',
+    },
   },
 }));
 
 const Index: FC = () => {
   const [wrapSSR, hashId] = useStyle();
+  const [showSide, setShowSide] = useState<boolean>(true);
 
   return wrapSSR(
     <div className={classNames('component-panel', hashId)}>
-      <ComponentTree />
-      <ButtonDemo />
+      <div
+        className={classNames('component-panel-side', {
+          'component-panel-side-hidden': !showSide,
+        })}
+      >
+        <ComponentTree />
+      </div>
+      <div className="component-panel-main">
+        <div className="component-panel-head">
+          <div className="component-panel-toggle-side-icon">
+            <MenuFoldOutlined onClick={() => setShowSide((prev) => !prev)} />
+          </div>
+        </div>
+      </div>
     </div>,
   );
 };
