7703a90bcd543f1de2c722c9561cdd411805543e
elrrrrrrr <elrrrrrrr@gmail.com>
2022-05-29 22:57:44 +0800

    Panel UI (#16)
    * feat: update style
    
    * feat: update copy
    
    * feat: update icons & sorts
    
    * fix: ci

--- src/token-panel/token-card/index.tsx
+++ src/token-panel/token-card/index.tsx
@@ -1,21 +1,22 @@
 import {
   AlignLeftOutlined,
   BgColorsOutlined,
-  FontSizeOutlined,
-  GatewayOutlined,
-  PlayCircleOutlined,
-  TabletOutlined,
   CaretRightOutlined,
+  FileUnknownOutlined,
+  FontColorsOutlined,
+  RadiusSettingOutlined,
+  TabletOutlined,
 } from '@ant-design/icons';
-import { Collapse } from '@madccc/antd';
+import { Collapse, Space } from '@madccc/antd';
 import '@madccc/antd/dist/@MadCcc/antd.css';
 import classNames from 'classnames';
 import type { ReactNode } from 'react';
 import React from 'react';
 import makeStyle from '../../utils/makeStyle';
 
-import TokenItem from '../token-item';
 import type { ThemeConfig } from '@madccc/antd/es/config-provider/context';
+import { Motion, ShapeLine } from '../../../icons';
+import TokenItem from '../token-item';
 
 const { Panel } = Collapse;
 
@@ -29,20 +30,23 @@ interface TokenCardProps {
 
 const IconMap: Record<string, ReactNode> = {
   color: <BgColorsOutlined />,
-  space: <GatewayOutlined />,
-  font: <FontSizeOutlined />,
+  space: <ShapeLine />,
+  font: <FontColorsOutlined />,
   line: <AlignLeftOutlined />,
   screen: <TabletOutlined />,
-  motion: <PlayCircleOutlined />,
+  motion: <Motion />,
+  radius: <RadiusSettingOutlined />,
+  others: <FileUnknownOutlined />,
 };
 export const TextMap: Record<string, string> = {
-  color: 'color 色彩',
-  space: 'space 间距',
-  font: 'font 字',
-  line: 'line 行',
-  screen: 'screen 屏幕',
-  motion: 'motion 动画',
-  else: '未分类',
+  color: 'Color 色彩',
+  space: 'Space 间距',
+  font: 'Font 文本',
+  line: 'Line 线',
+  screen: 'Screen 屏幕',
+  motion: 'Motion 动画',
+  radius: 'Radius 圆角',
+  others: 'Others 未分类',
 };
 
 const useStyle = makeStyle('TokenCard', (token) => ({
@@ -53,6 +57,34 @@ const useStyle = makeStyle('TokenCard', (token) => ({
     border: `1px solid ${token.colorBorder}`,
     marginBottom: token.margin,
   },
+
+  // FIXME antd collapse expandIconPosition not work
+  '.token-card .ant-collapse-icon-position-right .ant-collapse-header:not(.ant-collapse-icon-position-left .ant-collapse-header) > div:first-child':
+    {
+      position: 'absolute',
+      right: 8,
+    },
+
+  '.token-card .ant-input-group-wrapper': {
+    padding: '0 8px',
+  },
+
+  '.token-card .ant-input-group-wrapper .ant-input': {
+    background: 'white',
+    borderRadius: token.radiusLG,
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+  },
+
+  '.token-card .ant-input-group-addon': {
+    border: 0,
+    color: `rgba(0, 0, 0, 0.25)`,
+  },
+  '.token-card .ant-input-group >.ant-input:not(:first-child):not(:last-child)':
+    {
+      background: 'white',
+      borderRadius: token.radiusLG,
+    },
 }));
 
 export default ({ typeName, tokenArr }: TokenCardProps) => {
@@ -65,12 +97,14 @@ export default ({ typeName, tokenArr }: TokenCardProps) => {
         expandIcon={({ isActive }) => (
           <CaretRightOutlined rotate={isActive ? 90 : 0} />
         )}
+        expandIconPosition="right"
       >
         <Panel
           header={
-            <div>
-              {IconMap[typeName]} {TextMap[typeName]}
-            </div>
+            <Space size="small">
+              <span>{IconMap[typeName]}</span>
+              <span>{TextMap[typeName]}</span>
+            </Space>
           }
           key="1"
         >
