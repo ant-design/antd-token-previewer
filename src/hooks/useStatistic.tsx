07bf440cf2c9d80b06a39d4d63ebe364e5da8d31
MadCcc <1075746765@qq.com>
2022-05-31 16:57:37 +0800

    feat: use runtime statistic instead if build_statistic is empty (#20)
    * feat: use runtime statistic instead if build_statistic is empty
    
    * chore: update snapshot
    
    * chore: update files

--- src/hooks/useStatistic.tsx
+++ src/hooks/useStatistic.tsx
@@ -1,20 +1,28 @@
 import { useEffect, useState } from 'react';
-import { _statistic_build_ as statistic } from '@madccc/antd/lib/_util/theme/util/statistic';
+import {
+  _statistic_build_,
+  statistic as runtimeStatistic,
+} from '@madccc/antd/lib/_util/theme/util/statistic';
 
-const useStatistic = (selectedTokens: string[] = []) => {
-  const length = Object.keys(statistic).length;
+const useStatistic = (
+  selectedTokens: string[] = [],
+  statistic: typeof runtimeStatistic = runtimeStatistic,
+) => {
+  const mergedStatistic =
+    Object.keys(_statistic_build_).length === 0 ? statistic : _statistic_build_;
+  const length = Object.keys(mergedStatistic).length;
   const [relatedComponents, setRelatedComponents] = useState<string[]>([]);
 
   useEffect(() => {
     setRelatedComponents(
-      Object.entries(statistic)
+      Object.entries(mergedStatistic)
         .filter(([, tokens]) =>
           selectedTokens.some((item) => tokens.global.includes(item)),
         )
         .map(([component]) => component),
     );
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [length, selectedTokens.join(',')]);
+  }, [mergedStatistic, length, selectedTokens.join(',')]);
 
   return {
     relatedComponents,
