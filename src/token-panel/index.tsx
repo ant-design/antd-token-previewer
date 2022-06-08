d43efc8d7f6cc8ac5584b1946d30a5565da5f533
MadCcc <1075746765@qq.com>
2022-06-08 17:06:22 +0800

    feat: support sider drag

--- src/token-panel/index.tsx
+++ src/token-panel/index.tsx
@@ -9,27 +9,30 @@ import TokenCard, { IconMap, TextMap } from './token-card';
 import type { Theme } from '../interface';
 
 const useStyle = makeStyle('AliasTokenPreview', (token) => ({
-  '.preview-panel': {
-    height: '100%',
-    minWidth: 300,
-    backgroundColor: 'white',
-    display: 'flex',
-    flexDirection: 'column',
-    '.preview-panel-space': {
-      marginBottom: 20,
-      paddingInlineStart: token.paddingXS,
-    },
-    '.preview-panel-search': {
-      backgroundColor: 'rgba(0, 0, 0, 2%)',
-      borderRadius: token.radiusLG,
-      transition: `background-color ${token.motionDurationSlow}`,
-
-      input: {
-        fontSize: token.fontSizeSM,
+  '.preview-panel-wrapper': {
+    overflow: 'auto',
+    '.preview-panel': {
+      height: '100%',
+      minWidth: 300,
+      backgroundColor: 'white',
+      display: 'flex',
+      flexDirection: 'column',
+      '.preview-panel-space': {
+        marginBottom: 20,
+        paddingInlineStart: token.paddingXS,
       },
+      '.preview-panel-search': {
+        backgroundColor: 'rgba(0, 0, 0, 2%)',
+        borderRadius: token.radiusLG,
+        transition: `background-color ${token.motionDurationSlow}`,
 
-      '&:hover': {
-        backgroundColor: 'rgba(0, 0, 0, 4%)',
+        input: {
+          fontSize: token.fontSizeSM,
+        },
+
+        '&:hover': {
+          backgroundColor: 'rgba(0, 0, 0, 4%)',
+        },
       },
     },
   },
@@ -94,103 +97,105 @@ export default (props: TokenPreviewProps) => {
 
   return wrapSSR(
     <PreviewContext.Provider value={props}>
-      <div className={classNames('preview-panel', hashId)}>
-        <div style={{ padding: 16 }}>
-          <h3 className={classNames('preview-panel-space', hashId)}>
-            Alias Token 预览
-          </h3>
-          <Input
-            onChange={(e) => {
-              setSearch(e.target.value);
-            }}
-            bordered={false}
-            prefix={
-              <>
-                <Dropdown
-                  overlay={
-                    <Menu
-                      items={[
-                        {
-                          label: '筛选项',
-                          type: 'group',
-                          key: 'title-key',
-                          style: { fontSize: 12 },
-                        },
-                        ...TOKEN_SORTS.map((type) => ({
-                          icon: (
-                            <span>
-                              <CheckOutlined
-                                style={{
-                                  opacity: filterTypes.includes(type) ? 1 : 0,
-                                  marginRight: 8,
-                                  fontSize: 12,
-                                }}
-                              />
-                              {IconMap[type]}
-                            </span>
-                          ),
-                          label: TextMap[type],
-                          key: type,
-                          onClick: () => {
-                            setFilterTypes((prev) =>
-                              prev.includes(type)
-                                ? prev.filter((item) => type !== item)
-                                : [...prev, type],
-                            );
+      <div className={classNames('preview-panel-wrapper', hashId)}>
+        <div className={classNames('preview-panel')}>
+          <div style={{ padding: 16 }}>
+            <h3 className={classNames('preview-panel-space', hashId)}>
+              Alias Token 预览
+            </h3>
+            <Input
+              onChange={(e) => {
+                setSearch(e.target.value);
+              }}
+              bordered={false}
+              prefix={
+                <>
+                  <Dropdown
+                    overlay={
+                      <Menu
+                        items={[
+                          {
+                            label: '筛选项',
+                            type: 'group',
+                            key: 'title-key',
+                            style: { fontSize: 12 },
                           },
-                        })),
-                      ]}
-                    />
-                  }
-                  trigger={['click']}
-                >
-                  <SearchOutlined style={{ marginRight: 8 }} />
-                </Dropdown>
-                {filterTypes.map((item) => (
-                  <Tag
-                    color="#108ee9"
-                    key={item}
-                    closable
-                    onClose={() =>
-                      setFilterTypes((prev) =>
-                        prev.filter((type) => type !== item),
-                      )
+                          ...TOKEN_SORTS.map((type) => ({
+                            icon: (
+                              <span>
+                                <CheckOutlined
+                                  style={{
+                                    opacity: filterTypes.includes(type) ? 1 : 0,
+                                    marginRight: 8,
+                                    fontSize: 12,
+                                  }}
+                                />
+                                {IconMap[type]}
+                              </span>
+                            ),
+                            label: TextMap[type],
+                            key: type,
+                            onClick: () => {
+                              setFilterTypes((prev) =>
+                                prev.includes(type)
+                                  ? prev.filter((item) => type !== item)
+                                  : [...prev, type],
+                              );
+                            },
+                          })),
+                        ]}
+                      />
                     }
+                    trigger={['click']}
                   >
-                    {item}
+                    <SearchOutlined style={{ marginRight: 8 }} />
+                  </Dropdown>
+                  {filterTypes.map((item) => (
+                    <Tag
+                      color="#108ee9"
+                      key={item}
+                      closable
+                      onClose={() =>
+                        setFilterTypes((prev) =>
+                          prev.filter((type) => type !== item),
+                        )
+                      }
+                    >
+                      {item}
+                    </Tag>
+                  ))}
+                </>
+              }
+              className={classNames('preview-panel-search', hashId)}
+              placeholder="搜索 Token / 色值 / 文本 / 圆角等"
+            />
+            {selectedTokens.length > 0 && (
+              <div style={{ marginTop: 16 }}>
+                {selectedTokens.map((token) => (
+                  <Tag
+                    key={token}
+                    style={{ color: '#000' }}
+                    closable
+                    onClose={() => onTokenSelect(token)}
+                  >
+                    {token}
                   </Tag>
                 ))}
-              </>
-            }
-            className={classNames('preview-panel-search', hashId)}
-            placeholder="搜索 Token / 色值 / 文本 / 圆角等"
-          />
-          {selectedTokens.length > 0 && (
-            <div style={{ marginTop: 16 }}>
-              {selectedTokens.map((token) => (
-                <Tag
-                  key={token}
-                  style={{ color: '#000' }}
-                  closable
-                  onClose={() => onTokenSelect(token)}
-                >
-                  {token}
-                </Tag>
-              ))}
-            </div>
-          )}
-        </div>
-        <div style={{ flex: 1, overflow: 'auto', padding: '0 16px' }}>
-          {TOKEN_SORTS.filter(
-            (type) => filterTypes.includes(type) || filterTypes.length === 0,
-          ).map((key) => (
-            <TokenCard
-              key={key}
-              typeName={key}
-              tokenArr={groupedToken[key]}
-              keyword={search}
-            />
-          ))}
+              </div>
+            )}
+          </div>
+          <div style={{ flex: 1, overflow: 'auto', padding: '0 16px' }}>
+            {TOKEN_SORTS.filter(
+              (type) => filterTypes.includes(type) || filterTypes.length === 0,
+            ).map((key) => (
+              <TokenCard
+                key={key}
+                typeName={key}
+                tokenArr={groupedToken[key]}
+                keyword={search}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </PreviewContext.Provider>,
