bc27f875ac35eb3ab984fa6ce1e71b9daee6473d
MadCcc <1075746765@qq.com>
2022-06-22 23:03:20 +0800

    feat: support seed

--- src/previewer.tsx
+++ src/previewer.tsx
@@ -104,9 +104,9 @@ const useStyle = makeStyle('layout', (token) => ({
 const InternalPreviewer: React.FC<PreviewerProps> = ({ onSave }) => {
   const [wrapSSR, hashId] = useStyle();
   const [token] = useToken();
-  const [shownThemes, setShownThemes] = useState<string[]>(['light', 'dark']);
+  const [shownThemes, setShownThemes] = useState<string[]>(['default', 'dark']);
   const [enabledThemes, setEnabledThemes] = useState<string[]>([
-    'light',
+    'default',
     'dark',
   ]);
   const [selectedTokens, setSelectedTokens] = useState<TokenName[]>([]);
@@ -126,20 +126,20 @@ const InternalPreviewer: React.FC<PreviewerProps> = ({ onSave }) => {
 
   const [themes, setThemes] = useState<ThemeSelectProps['themes']>([
     {
-      name: '亮色主题',
-      key: 'light',
-      config: {
-        override: {
-          alias: { ...token, ...lightAliasToken },
-          ...lightComponentToken,
-        },
-      },
+      name: '默认主题',
+      key: 'default',
+      config: {},
       fixed: true,
     },
     // {
-    //   name: '默认主题',
-    //   key: 'default',
-    //   config: { override: { alias: token } },
+    //   name: '亮色主题',
+    //   key: 'light',
+    //   config: {
+    //     override: {
+    //       alias: { ...lightAliasToken },
+    //       ...lightComponentToken,
+    //     },
+    //   },
     //   fixed: true,
     // },
     {
