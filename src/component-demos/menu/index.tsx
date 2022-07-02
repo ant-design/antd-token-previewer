be238deb13a84a66aca355d2d87dc6514b40c133
arvinxx <arvinx@foxmail.com>
2022-07-02 23:57:52 +0800

    :sparkles: feat: 增加若干组件 demo

--- src/component-demos/menu/index.tsx
+++ src/component-demos/menu/index.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import Default from './menu';
 
 import danger from './menuDanger';
+import MenuInLayout from './menuInLayout';
 
 import type { PreviewerDemo } from '../../interface';
 
 const previewerDemo: PreviewerDemo = {
   default: <Default />,
-  optional: [danger],
+  optional: [danger, MenuInLayout],
 };
 
 export default previewerDemo;
