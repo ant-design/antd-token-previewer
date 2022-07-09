58ddfe611e7847492547a1a6b4cda782dcbabbb3
arvinxx <arvinx@foxmail.com>
2022-07-09 15:09:43 +0800

    :sparkles: feat: 新增文本组件 demo

--- src/component-demos/typography/index.tsx
+++ src/component-demos/typography/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import TypographyDemo from './typography';
 import Heading4 from './Heading4';
 import warning from './warning';
+import success from './success';
 import error from './error';
 
 import type { ComponentDemo } from '../../interface';
@@ -12,6 +13,7 @@ const previewerDemo: ComponentDemo[] = [
   Heading4,
   error,
   warning,
+  success,
 ];
 
 export default previewerDemo;
---
+++ src/component-demos/typography/success.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Typography } from 'antd';
+import type { ComponentDemo } from '../../interface';
+
+const { Title, Text } = Typography;
+
+const Demo = () => (
+  <div>
+    <Title type={'success'} level={4}>
+      Success Title
+    </Title>
+    <Text type={'success'}>success Text</Text>
+  </div>
+);
+
+const componentDemo: ComponentDemo = {
+  demo: <Demo />,
+  tokens: ['colorSuccess'],
+};
+
+export default componentDemo;
