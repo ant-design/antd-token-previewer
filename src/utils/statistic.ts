3839ee5d21eab8ec2f8ce7fae7dcfe8f53664654
MadCcc <1075746765@qq.com>
2022-07-25 17:12:33 +0800

    feat: map component count

--- src/utils/statistic.ts
+++ src/utils/statistic.ts
@@ -1,15 +1,32 @@
 import tokenStatistic from 'antd/es/version/token';
+import type { TokenName } from '../interface';
 
-export const getRelatedComponents = (token: string | string[]): string[] => {
-  return Object.entries(tokenStatistic)
-    .filter(([, tokens]) => {
-      if (typeof token === 'string') {
+const tokenRelatedComponents: {
+  [key in TokenName]?: string[];
+} = {};
+
+const getRelatedComponentsSingle = (token: TokenName): string[] => {
+  if (!tokenRelatedComponents[token]) {
+    tokenRelatedComponents[token] = Object.entries(tokenStatistic)
+      .filter(([, tokens]) => {
         return (tokens.global as string[]).includes(token);
-      } else {
-        return token.some((item) => (tokens.global as string[]).includes(item));
-      }
-    })
-    .map(([component]) => component);
+      })
+      .map(([component]) => component);
+  }
+  return tokenRelatedComponents[token] ?? [];
+};
+
+export const getRelatedComponents = (
+  token: TokenName | TokenName[],
+): string[] => {
+  const mergedTokens = Array.isArray(token) ? token : [token];
+  return Array.from(
+    new Set(
+      mergedTokens.reduce<string[]>((result, item) => {
+        return result.concat(getRelatedComponentsSingle(item));
+      }, []),
+    ),
+  );
 };
 
 export const getComponentToken = (component: string) =>
