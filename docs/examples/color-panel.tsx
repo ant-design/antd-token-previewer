58df855981d9ec9a8a67b96443fd7895afc68321
MadCcc <1075746765@qq.com>
2022-08-03 17:55:07 +0800

    feat: color picker (#87)
    * feat: color picker
    
    * test: fix lint
    
    * feat: color font-family
    
    * chore: code update
    
    * chore: code clean
    
    * feat: color fallback font family

---
+++ docs/examples/color-panel.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'antd/es/style/reset.css';
+import ColorPanel from '../../src/ColorPanel';
+
+const Demo = () => {
+  const [color, setColor] = React.useState<string>('#1632ff');
+
+  return (
+    <React.StrictMode>
+      <ColorPanel alpha color={color} onChange={(v) => setColor(v)} />
+    </React.StrictMode>
+  );
+};
+
+export default Demo;
