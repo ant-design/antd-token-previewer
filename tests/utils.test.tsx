49853fc5f26eb7d3a8f4055ff1da511ca9e20f78
MadCcc <1075746765@qq.com>
2022-08-18 10:52:34 +0800

    test: add getDesignToken test case

--- tests/utils.test.tsx
+++ tests/utils.test.tsx
@@ -1,10 +1,16 @@
-import type { TokenEntity } from '../src/interface';
+import { render } from '@testing-library/react';
+import { ConfigProvider, theme } from 'antd';
+import { getDesignToken } from 'antd-token-previewer';
 import type { ThemeConfig } from 'antd/es/config-provider/context';
+import React from 'react';
+import type { TokenEntity } from '../src/interface';
 import {
   convertTokenArrToConfig,
   convertTokenConfigToArr,
 } from '../src/utils/convertToken';
 import getValueByPath from '../src/utils/getValueByPath';
+
+const { useToken, darkAlgorithm } = theme;
 
 describe('Utils', () => {
   describe('convertToken', () => {
@@ -77,5 +83,35 @@ describe('Utils', () => {
     it('null object should return null', () => {
       expect(getValueByPath(null, ['a', 'b'])).toBeUndefined();
     });
+  });
+
+  it('getDesignToken should be consistent with useToken', () => {
+    const themeConfig: ThemeConfig = {
+      token: {
+        colorPrimary: '#f5f5',
+      },
+      algorithm: darkAlgorithm,
+      override: {
+        alias: {
+          colorLink: '#66ccff',
+        },
+      },
+    };
+    let tokenFromHook;
+
+    const Component = () => {
+      const { token } = useToken();
+      tokenFromHook = token;
+      delete (tokenFromHook as any)._hashId;
+      delete (tokenFromHook as any)._tokenKey;
+      return null;
+    };
+
+    render(
+      <ConfigProvider theme={themeConfig}>
+        <Component />
+      </ConfigProvider>,
+    );
+    expect(getDesignToken(themeConfig)).toStrictEqual(tokenFromHook);
   });
 });
