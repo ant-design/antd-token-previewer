e9fb5e0942887820a2d7d5c7b82360bf6386f1c1
MadCcc <1075746765@qq.com>
2022-08-23 23:43:58 +0800

    feat: component alias (#107)

--- docs/examples/theme-editor-simple.tsx
+++ docs/examples/theme-editor-simple.tsx
@@ -2,15 +2,18 @@
  * iframe: 800
  */
 
-import { ConfigProvider } from 'antd';
+import { Button, ConfigProvider, message } from 'antd';
 import type { Theme } from 'antd-token-previewer';
 import { ThemeEditor } from 'antd-token-previewer';
 import 'antd/es/style/reset.css';
-import React from 'react';
+import React, { useEffect } from 'react';
+
+const ANT_DESIGN_V5_CUSTOM_THEME_PRO = 'ant-design-v5-custom-theme-pro';
 
 const Demo = () => {
+  const [messageApi, contextHolder] = message.useMessage();
   const [theme, setTheme] = React.useState<Theme>({
-    name: '秘制猪蹄',
+    name: '自定义主题',
     key: 'secret theme',
     config: {
       token: {
@@ -24,9 +27,52 @@ const Demo = () => {
     setTheme(newTheme);
   };
 
+  useEffect(() => {
+    const storedConfig = localStorage.getItem(ANT_DESIGN_V5_CUSTOM_THEME_PRO);
+    if (storedConfig) {
+      setTheme((prev) => ({ ...prev, config: JSON.parse(storedConfig) }));
+    }
+  }, []);
+
+  const handleSave = () => {
+    localStorage.setItem(
+      ANT_DESIGN_V5_CUSTOM_THEME_PRO,
+      JSON.stringify(theme.config),
+    );
+    messageApi.success('保存成功');
+  };
+
   return (
     <React.StrictMode>
+      {contextHolder}
       <ConfigProvider theme={{ hashed: true }}>
+        <div
+          style={{
+            display: 'flex',
+            height: 56,
+            alignItems: 'center',
+            padding: '0 24px',
+            justifyContent: 'space-between',
+            borderBottom: '1px solid #F0F0F0',
+          }}
+        >
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div
+              style={{
+                marginRight: 8,
+                display: 'inline-block',
+                width: 32,
+                height: 32,
+                borderRadius: 4,
+                background: theme.config.token?.colorPrimary,
+              }}
+            />
+            <span>{theme.name}</span>
+          </div>
+          <Button type="primary" onClick={handleSave}>
+            保存
+          </Button>
+        </div>
         <ThemeEditor
           simple
           theme={theme}
