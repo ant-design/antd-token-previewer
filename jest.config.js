7ff609b85acda8f75cb90e36e7cc15975b072977
MadCcc <1075746765@qq.com>
2022-08-30 17:11:17 +0800

    test: mock default hashed

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   verbose: true,
   testEnvironment: 'jsdom',
   setupFiles: ['./tests/utils/setup.js'],
+  setupFilesAfterEnv: ['./tests/utils/setupAfterEnv.ts'],
   transformIgnorePatterns: [
     'node_modules/(?!.*@(babel|antd))(?!array-move)[^/]+?/(?!(es|node_modules)/)',
   ],
