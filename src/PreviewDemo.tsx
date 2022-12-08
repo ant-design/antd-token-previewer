47f4335ffc927e07fa61db4d2247af3605be5abf
MadCcc <1075746765@qq.com>
2022-12-08 20:32:08 +0800

    chore: style

--- src/PreviewDemo.tsx
+++ src/PreviewDemo.tsx
@@ -16,6 +16,7 @@ const PreviewDemo: FC<PreviewDemoProps> = ({ theme, style }) => {
         theme={theme}
         components={antdComponents}
         componentDrawer={false}
+        style={{ minHeight: '100%' }}
       />
     </div>
   );
