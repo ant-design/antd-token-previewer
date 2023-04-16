39413af94b7c3033afadcc70a83ae6e579a1c74f
heiyu4585 <wangning4567@163.com>
2023-04-16 21:33:56 +0800

    ✨ feat: update editor

--- docs/JSONEditor/index.tsx
+++ docs/JSONEditor/index.tsx
@@ -3,12 +3,12 @@ import type { JSONEditorPropsOptional } from 'vanilla-jsoneditor';
 import { JSONEditor, Mode } from 'vanilla-jsoneditor';
 
 const Editor: React.FC<JSONEditorPropsOptional> = (props) => {
-  const editorRef = useRef<JSONEditor>(null);
+  const editorRef = useRef<JSONEditor | null>(null);
   const container = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     editorRef.current = new JSONEditor({
-      target: container.current,
+      target: container.current as HTMLDivElement,
       props: { mode: Mode.text },
     });
     return () => {
