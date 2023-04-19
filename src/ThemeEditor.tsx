03e65785c1b42a8424483ec3ab5150645e9590ea
黑雨 <wangning4567@163.com>
2023-04-19 11:42:56 +0800

    feat: add componets JSONEditor (#173)
    * ✨ feat: init
    
    * ✨ feat: init
    
    * ✨ feat: init
    
    * ✨ feat: update editor
    
    * ✨ feat: init
    
    * ✨ feat: init
    
    * ✨ feat: init
    
    * ✨ feat: components add jsonEditor
    
    * 🔥 feat: clear up
    
    * 💄 style: clear up
    
    * ✨ feat: update test
    
    * ♻️ feat: update
    
    * 🗑️ feat: update

--- src/ThemeEditor.tsx
+++ src/ThemeEditor.tsx
@@ -1,13 +1,22 @@
 import type { DerivativeFunc } from '@ant-design/cssinjs';
 import { CaretDownOutlined } from '@ant-design/icons';
-import { Dropdown, Segmented, Tag } from 'antd';
+import { Button, Dropdown, message, Modal, Segmented, Tag } from 'antd';
+import type { ThemeConfig } from 'antd/es/config-provider/context';
 import classNames from 'classnames';
 import useMergedState from 'rc-util/lib/hooks/useMergedState';
 import type { ReactNode } from 'react';
-import React, { forwardRef, useImperativeHandle } from 'react';
+import React, {
+  forwardRef,
+  useCallback,
+  useImperativeHandle,
+  useMemo,
+  useState,
+} from 'react';
+import type { OnChange } from 'vanilla-jsoneditor';
 import GlobalTokenEditor from './GlobalTokenEditor';
 import useControlledTheme from './hooks/useControlledTheme';
 import type { Theme } from './interface';
+import JSONEditor from './JSONEditor';
 import type { Locale } from './locale';
 import { LocaleContext, zhCN } from './locale';
 import makeStyle from './utils/makeStyle';
@@ -69,6 +78,10 @@ export type ThemeEditorProps = {
   onAdvancedChange?: (advanced: boolean) => void;
 };
 
+function isObject(target: any) {
+  return Object.prototype.toString.call(target) === '[object Object]';
+}
+
 const ThemeEditor = forwardRef<ThemeEditorRef, ThemeEditorProps>(
   (
     {
@@ -88,10 +101,21 @@ const ThemeEditor = forwardRef<ThemeEditorRef, ThemeEditorProps>(
   ) => {
     const prefixCls = 'antd-theme-editor';
     const [wrapSSR, hashId] = useStyle(prefixCls);
-
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [editThemeFormatRight, setEditThemeFormatRight] =
+      useState<boolean>(true);
     const [mode, setMode] = useMergedState<ThemeEditorMode>('global', {
       value: customMode,
       onChange: onModeChange,
+    });
+    const [messageApi, contextHolder] = message.useMessage();
+
+    const [themeConfigContent, setThemeConfigContent] = useState<{
+      text: string;
+      json?: undefined;
+    }>({
+      text: JSON.stringify(customTheme?.config ?? defaultTheme.config),
+      json: undefined,
     });
 
     const handleAdvancedChange = (value: boolean) => {
@@ -110,16 +134,76 @@ const ThemeEditor = forwardRef<ThemeEditorRef, ThemeEditorProps>(
       useControlledTheme({
         theme: customTheme,
         defaultTheme,
-        onChange: onThemeChange,
+        onChange: (newTheme: Theme) => {
+          onThemeChange?.(newTheme);
+          setThemeConfigContent({
+            text: JSON.stringify(newTheme.config),
+          });
+        },
         darkAlgorithm,
       });
+
+    const editTotal = useMemo(() => {
+      const { token = {}, components = {} } = theme.config;
+      let mergedEditTotal = Object.keys(token).length;
+      if (components) {
+        for (const key in components) {
+          const mergedItem = components[key as keyof ThemeConfig['components']];
+          if (mergedItem && isObject(mergedItem)) {
+            mergedEditTotal += Object.keys(mergedItem).length;
+          }
+        }
+      }
+      return mergedEditTotal;
+    }, [theme]);
 
     useImperativeHandle(ref, () => ({
       updateRef,
     }));
 
+    const editModelClose = () => {
+      setIsModalOpen(false);
+    };
+
+    const handleEditConfigChange: OnChange = (
+      newContent,
+      preContent,
+      status,
+    ) => {
+      setThemeConfigContent(newContent as { text: string });
+      if (
+        status?.contentErrors &&
+        Object.keys(status.contentErrors).length > 0
+      ) {
+        setEditThemeFormatRight(false);
+      } else {
+        setEditThemeFormatRight(true);
+      }
+    };
+
+    const editSave = useCallback(() => {
+      if (!editThemeFormatRight) {
+        messageApi.error('主题 JSON 格式错误');
+        return;
+      }
+
+      const themeConfig = {
+        ...theme,
+        config: JSON.parse(themeConfigContent.text),
+      };
+
+      if (!isObject(themeConfig)) {
+        messageApi.error('主题 JSON 格式错误');
+        return;
+      }
+      onThemeChange?.(themeConfig);
+      editModelClose();
+      messageApi.success('编辑成功');
+    }, [themeConfigContent]);
+
     return wrapSSR(
       <LocaleContext.Provider value={locale}>
+        {contextHolder}
         <div className={classNames(hashId, prefixCls, className)} style={style}>
           <div className={`${prefixCls}-header`}>
             <div className={`${prefixCls}-header-title`}>{locale.title}</div>
@@ -158,7 +242,18 @@ const ThemeEditor = forwardRef<ThemeEditorRef, ThemeEditorProps>(
                 style={{ marginLeft: 24 }}
               />
             )}
-            <div className={`${prefixCls}-header-actions`}>{actions}</div>
+            <div className={`${prefixCls}-header-actions`}>
+              <span style={{ marginRight: 8 }}>
+                共 <span style={{ color: '#dd5b21' }}>{editTotal}</span> 处修改
+              </span>
+              <Button
+                style={{ marginRight: 8 }}
+                onClick={() => setIsModalOpen(true)}
+              >
+                主题配置
+              </Button>
+              {actions}
+            </div>
           </div>
           <div className={`${prefixCls}-body`}>
             {mode === 'global' && (
@@ -170,6 +265,18 @@ const ThemeEditor = forwardRef<ThemeEditorRef, ThemeEditorProps>(
               />
             )}
           </div>
+          <Modal
+            title="主题配置"
+            open={isModalOpen}
+            onOk={editSave}
+            onCancel={editModelClose}
+          >
+            <JSONEditor
+              content={themeConfigContent}
+              onChange={handleEditConfigChange}
+              mainMenuBar={false}
+            />
+          </Modal>
         </div>
       </LocaleContext.Provider>,
     );
