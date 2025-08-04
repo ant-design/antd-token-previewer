d00fcabc039afa9c746a8aaf07ab303b88dd7db5
EmilyyyLiu <100924403+EmilyyyLiu@users.noreply.github.com>
2025-08-04 14:08:30 +0800

    fix: The Ghost Button of the theme is not visible (#218)
    Co-authored-by: 刘欢 <lh01217311@antgroup.com>

--- src/previews/components/button/button.tsx
+++ src/previews/components/button/button.tsx
@@ -1,16 +1,28 @@
-import { Button, Space } from 'antd';
+import { Button, Flex, Space } from 'antd';
 import React from 'react';
 import type { ComponentDemo } from '../../../interface';
 
 const Demo = () => (
-  <Space>
-    <Button type="primary">Primary Button</Button>
-    <Button>Default Button</Button>
-    <Button type="dashed">Dashed Button</Button> <br />
-    <Button type="text">Text Button</Button>
-    <Button ghost>Ghost Button</Button>
-    <Button type="link">Link Button</Button>
-  </Space>
+  <>
+    <Space wrap>
+      <Button type="primary">Primary Button</Button>
+      <Button>Default Button</Button>
+      <Button type="dashed">Dashed Button</Button> <br />
+      <Button type="text">Text Button</Button>
+      <Button type="link">Link Button</Button>
+    </Space>
+    <Flex
+      justify="center"
+      align="center"
+      style={{
+        backgroundColor: 'rgb(190,200,200)',
+        height: 50,
+        marginTop: 16,
+      }}
+    >
+      <Button ghost>Ghost Button</Button>
+    </Flex>
+  </>
 );
 
 const componentDemo: ComponentDemo = {
